82bf18ba6f6065f166c10da8e94c4325f233c658
Fabio <murphy84@tiscali.it>
2026-09-07 10:11:23 +0200

    Modifiche a open_rdflib

--- tools.py
+++ tools.py
@@ -1,37 +1,17 @@
 from pathlib import Path
+from rdflib import Graph
 
-from rdflib import Graph, Namespace
-from rdflib.namespace import DCTERMS, FOAF, RDF, RDFS
-
-
-CC = Namespace("http://creativecommons.org/ns#")
-ODRL = Namespace("http://www.w3.org/ns/odrl/2/")
-RDFLICENSE = Namespace("http://purl.org/NET/rdflicense/")
-SPDX = Namespace("http://spdx.org/rdf/terms#")
-
-
-# Questa funzione dovrebbe aprire un file json+ld (comincia con cc-by4.0)
-# nella cartella policies utilizzando la libreria rdflib e stampare qualcosa
-# per verificare che il file è stato aperto correttamente
+# Apro un file Json+ld con la libreria RDFLib
 def open_rdflib(file: str):
     project_root = Path(__file__).resolve().parent
     path = project_root / file
 
     graph = Graph()
-    graph.bind("cc", CC)
-    graph.bind("dct", DCTERMS)
-    graph.bind("foaf", FOAF)
-    graph.bind("odrl", ODRL)
-    graph.bind("rdf", RDF)
-    graph.bind("rdfs", RDFS)
-    graph.bind("rdflicense", RDFLICENSE)
-    graph.bind("spdx", SPDX)
     graph.parse(path, format="json-ld")
 
-    ns = graph.namespace_manager
     print(f"Opened {path.name}: {len(graph)} triples")
     for s, p, o in graph:
-        print(f"  {s.n3(ns)} {p.n3(ns)} {o.n3(ns)}")
+        print(f"  {s} {p} {o}")
 
     return graph
 
