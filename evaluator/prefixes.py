19ec74601e89cb272771e48e34e630fc2948942e
Fabio Marfia <marfif@usi.ch>
2026-09-12 17:07:05 +0200

    Aggiunta delle policy e request di esempio per l'evaluator

--- evaluator/prefixes.py
+++ evaluator/prefixes.py
@@ -6,4 +6,10 @@ prefixes = {
     "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
     "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
     "spdx": "http://spdx.org/rdf/terms#",
+    "purl": "http://purl.org/NET/rdflicense/",
+    "ex_policy": "http://example.com/policy/",
+    "ex_rule": "http://example.com/rule/",
+    "ex_document": "http://example.com/document/",
+    "ex_party": "http://example.com/party/",
+    "ex_constraint": "http://example.com/constraint/",
 }
